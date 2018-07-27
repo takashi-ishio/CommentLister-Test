d881e5e505be18155316ac7a2a708cad6bf351c3
Takashi Ishio <ishio@is.naist.jp>
2018-07-27 09:44:39 +0900

    Deleted and Added

--- src/example/C.java
+++ src/example/C.java
@@ -1,7 +1,6 @@
 package example;
 
 /**
- * http://github.com/takashi-ishio/CommentLister-Test
  * Example project using comments
  * @author ishio
  * 
@@ -10,5 +9,7 @@ public class C {
 
 	// this url does not work
 	private static final String url = "http://github.com/takashi-ishio/CommentLister-Test";
-	
+
+	// http://github.com/takashi-ishio/CommentLister-Test
+
 }
