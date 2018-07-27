2fe221a11d3c485861317e9747d02abec74b807e
Takashi Ishio <ishio@is.naist.jp>
2018-07-27 14:22:32 +0900

    Replaced URLs

--- src/example/F.java
+++ src/example/F.java
@@ -8,7 +8,7 @@ public class F {
 	// TEST http://github.com/takashi-ishio/
 
 	// Two URLs
-	// http://github.com/takashi-ishio/3
 	// http://github.com/takashi-ishio/4
+	// http://github.com/takashi-ishio/3
 
 }
