507fc7368cbb10ea1482d1738e231c333c0db99b
Takashi Ishio <ishio@is.naist.jp>
2018-07-27 09:41:51 +0900

    Initial commit

---
+++ src/example/C.java
@@ -0,0 +1,14 @@
+package example;
+
+/**
+ * Example project using comments
+ * @author ishio
+ * http://github.com/takashi-ishio/CommentLister-Test
+ * 
+ */
+public class C {
+
+	// this url does not work
+	private static final String url = "http://github.com/takashi-ishio/CommentLister-Test";
+	
+}
