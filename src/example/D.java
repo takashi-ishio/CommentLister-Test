b879cce4c57ffad8e3daf5cf79d3a7e8dad8fe10
Takashi Ishio <ishio@is.naist.jp>
2018-07-27 09:42:55 +0900

    Added new URLs

--- src/example/D.java
+++ src/example/D.java
@@ -4,9 +4,12 @@ package example;
  * Example project using comments
  * @author ishio
  * http://github.com/takashi-ishio/CommentLister-Test
+ * http://github.com/takashi-ishio/CommentLister-Test2
  * 
  */
 public class D {
+
+	// http://github.com/takashi-ishio/CommentLister
 
 	// this url does not work
 	private static final String url = "http://github.com/takashi-ishio/CommentLister-Test";
